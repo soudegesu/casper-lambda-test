b75f3ee2949ab714b2eaa311dc16f0c189bda4a2
Takaaki Suzuki <taa-suzuki@kddi.com>
2018-04-24 07:53:04 +0900

    edit casperjs script

--- src/scripts/sample-script.js
+++ src/scripts/sample-script.js
@@ -1,9 +1,23 @@
 // Simple Javascript example..
-var casper = require('casper').create();
+const casper = require('casper').create({
+    verbose: true,
+    viewportSize: {
+        width: 1440,
+        height: 900
+    }
+});
 
-var args = {
-    email: casper.cli.get('email'),
-    password: casper.cli.get('password')
+// Change browser lang
+casper.on('started', function () {
+    this.page.customHeaders = {
+        "Accept-Language": "en-US"
+    }
+});
+
+// argument from index.js
+const args = {
+    upload_file: casper.cli.get('upload_file'),
+    tmp: casper.cli.get('tmp')
 };
 
 console.log('parameters data:', JSON.stringify(args));
@@ -12,14 +26,19 @@ console.log('parameters data:', JSON.stringify(args));
 var url1 = 'http://casperjs.org/';
 var url2 = 'http://phantomjs.org';
 
+/////
+// write your logic
+/////
 console.log('Loading a web page: ' + url1);
 casper.start(url1, function() {
     this.echo('Page title: ' + this.getTitle());
+    this.capture(tmp + '01-casper.png');
 });
 
 console.log('Loading a web page: ' + url2);
 casper.thenOpen(url2, function() {
     this.echo('Page title: ' + this.getTitle());
+    this.capture(tmp + '02-phantom.png');
 });
 
 casper.run(function() {
